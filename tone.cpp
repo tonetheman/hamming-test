a117c1cc3adc990f6f1e1671acd58a429e36f239
agcc <agcc@agcc-desktop.(none)>
2009-07-19 00:26:37 -0500

    compiles does not run

--- tone.cpp
+++ tone.cpp
@@ -16,6 +16,11 @@ const int CUTOFF = 2;
 typedef vector<string> VS;
 typedef vector<int> VI;
 
+int make_rand() {
+	return rand() % 1000;
+}
+
+
 
 struct SHA {
 	public:
@@ -61,6 +66,54 @@ struct SHA {
 	}	
 };
 
+struct Words {
+	VS words;
+	void init() {
+		words = load_file();
+	}
+	int size() {
+		return words.size();
+	}
+	VS load_file() {
+		ifstream inf;
+		inf.open("./words");
+		if (!inf) {
+			cout << "unable to open word list" << endl;
+			exit(1);
+		}
+		string buffer;
+		VS result;
+		while(inf >> buffer) {
+			result.push_back(buffer);
+		}
+		inf.close();
+		return result;
+	}
+
+	string get_random_word() {
+		return words[make_rand()];
+	}
+
+	string make_target_string() {
+		string ts;
+		for(int i=0;i<WORD_COUNT;i++) {
+			ts += get_random_word();
+			if (i==WORD_COUNT-1) {
+				// do nothing
+			} else {
+				ts += " ";
+			}	
+		}
+		return ts;
+	}
+	string get(int index) {
+		return words[index];
+	}
+	SHA make_target_sha(const string& s) {
+		return SHA::sha_it(s.c_str());
+	}
+};
+
 ostream& operator<<(ostream& os, VS& vs) {
 	VS::iterator ip = vs.begin();
 	while(ip!=vs.end()) {
@@ -69,16 +122,15 @@ ostream& operator<<(ostream& os, VS& vs) {
 	return os;
 }
 
-int make_rand();
 
 struct Population {
 	VS population[POPULATION_SIZE];
 
-	void init(VS& words) {
+	void init(Words& words) {
 		for(int i=0;i<POPULATION_SIZE;i++) {
 			for(int j=0;j<WORD_COUNT;j++) {
 				int choice = make_rand();
-				population[i].push_back(words[choice]);
+				population[i].push_back(words.get(choice));
 			}	
 		}
 	}
@@ -113,52 +165,13 @@ struct Population {
 			pop_score[i] = tmp_sha_score;
 		}
 	}
-};
 
-struct Words {
-	VS words;
-	void init() {
-		words = load_file();
-	}
-	int size() {
-		return words.size();
-	}
-	VS load_file() {
-		ifstream inf;
-		inf.open("./words");
-		if (!inf) {
-			cout << "unable to open word list" << endl;
-			exit(1);
+	void clear() {
+		for(int i=0;i<POPULATION_SIZE;i++) {
+			population[i].clear();
 		}
-		string buffer;
-		VS result;
-		while(inf >> buffer) {
-			result.push_back(buffer);
-		}
-		inf.close();
-		return result;
 	}
 
-	string get_random_word(VS& vs) {
-		return vs[make_rand()];
-	}
-
-	string make_target_string(VS& words) {
-		string ts;
-		for(int i=0;i<WORD_COUNT;i++) {
-			ts += get_random_word(words);
-			if (i==WORD_COUNT-1) {
-				// do nothing
-			} else {
-				ts += " ";
-			}	
-		}
-		return ts;
-	}
-
-	SHA make_target_sha(const string& s) {
-		return SHA::sha_it(s.c_str());
-	}
 };
 
 struct SimData {
@@ -166,6 +179,7 @@ struct SimData {
 	string target;
 	SHA target_sha;
 	Words words;
+	Population population;
 
 	int current_low_index;
 	VS current_low;
@@ -176,11 +190,69 @@ struct SimData {
 	void load_data() {
 		words.load_file();
 	}
-};
+	string make_target_string() {
+		target = words.make_target_string();
+		return target;
+	}
+	SHA make_target_sha() {
+		target_sha = words.make_target_sha(target);
+		return target_sha;
+	}
+	int find_low(int* pop_score) {
+		int index = -1;
+		int low_score = 10000;
+		for(int i=0;i<POPULATION_SIZE;i++) {
+			if (pop_score[i] < low_score) {
+				index = i;
+				low_score = pop_score[i];
+			}
+		}
+		return index;
+	}
 
-int make_rand() {
-	return rand() % 1000;
-}
+
+	void sim_loop() {
+		int pop_score[POPULATION_SIZE];
+		int lowest_score = 1024;
+		int count = 0;
+		population.init(words);
+
+		while(true) {
+			population.score_population(target_sha, pop_score);
+			int new_low_index = find_low(pop_score);
+
+			if (new_low_index<lowest_score) {
+				lowest_score = pop_score[current_low_index];
+				this->current_low_index = new_low_index;
+				this->current_low = population.get(current_low_index);
+				this->current_low_score = pop_score[current_low_index];
+				write_current_low();
+			}
+
+			population.clear();
+			population.init(words);
+
+			count++;
+			if (count>CUTOFF) break;
+		}
+
+		cout << "final low is " << current_low << endl;
+		cout << "score for final low is " << current_low_score << endl;
+	}
+	void write_current_low() {
+		ofstream outf;
+		outf.open("./results", ios::app);
+		VS::iterator it = current_low.begin();
+		while(it!=current_low.end()) {
+			outf << *it;
+			it++;
+		}
+		outf << endl;
+		outf << "low score: " << current_low_score << endl;	
+		outf.close();
+	}
+
+};
 
 ostream& operator<<(ostream& os, SHA& sha) {
 	for(int i=0;i<20;i++) {
@@ -189,63 +261,17 @@ ostream& operator<<(ostream& os, SHA& sha) {
 	return os;
 }
 
-SHA sha_it(const char * cp) {
-	sha1_context ctx;
-	sha1_starts(&ctx);
-	sha1_update(&ctx,(unsigned char*)cp, strlen(cp));
-	SHA s;
-	sha1_finish(&ctx, s.sha1sum);
-	return s;
-}
-
-void sha_print(SHA s) {
-	for(int i=0;i<20;i++) {
-		cout << hex << (int)s.sha1sum[i];
-	}
-	cout << endl;
-}
-
-int __score(unsigned char c) {
-	int sum = 0;
-        switch(c) {
-        	case 0x1:
-                case 0x2: case 0x4: case 0x8:
-                	sum +=1; break;
-                case 0x3: case 0x5: case 0x6: case 0x9:
-                case 10: case 12: 
-                        sum +=2; break;
-                case 0x7: case 14: case 13: case 11:
-                        sum += 3; break;
-                case 15:
-                        sum +=4; break;
-                }
-	return sum;
-}
-
-int score(SHA s1, SHA s2) {
-	SHA result;
-	int sum = 0;
-	for(int i=0;i<20;i++ ) {
-		result.sha1sum[i] = s1.sha1sum[i] ^ s2.sha1sum[i];
-		unsigned char part1 = result.sha1sum[i] >> 4;
-		unsigned char part2 = (unsigned char)(result.sha1sum[i] & 0x0f);
-		sum += __score(part1);
-		sum += __score(part2);
-	}
-	return sum;
-}
-
 void test() {
 	const char * cp = "Rubinius one eight six active active record memcached exception JRuby DHH TOKYO sdfe3";
-	SHA sha = sha_it(cp);
-	sha_print(sha);
+	SHA sha = SHA::sha_it(cp);
+	cout << sha << endl;
 
 	const char * cp1 = "I am not a big believer in fortune telling";
-	SHA sha1 = sha_it(cp1);
-	sha_print(sha1);
+	SHA sha1 = SHA::sha_it(cp1);
+	cout << sha1 << endl;
 
 	// hamming testing
-	cout << "score: " << dec << score(sha,sha1) << endl;
+	cout << "score: " << dec << SHA::score(sha,sha1) << endl;
 }
 
 void init() {
@@ -271,88 +297,17 @@ void report_score(int * pop_score) {
 	}
 }
 
-int find_low(int* pop_score) {
-	int index = -1;
-	int low_score = 10000;
-	for(int i=0;i<POPULATION_SIZE;i++) {
-		if (pop_score[i] < low_score) {
-			index = i;
-			low_score = pop_score[i];
-		}
-	}
-	return index;
-}
-
-void setup_new_population(SimData& sim) {
-	VS good_stuff = population[sim.current_low_index];
-
-	for(int i=0;i<POPULATION_SIZE;i++) {
-		population[i].clear();	
-	}
-	initialize_population(sim.words);
-}
-
-void write_current_low(SimData& sim) {
-	ofstream outf;
-	outf.open("./results", ios::app);
-	VS::iterator it = sim.current_low.begin();
-	while(it!=sim.current_low.end()) {
-		outf << *it;
-		it++;
-	}
-	outf << endl;
-	outf << "low score: " << sim.current_low_score << endl;	
-	outf.close();
-}
-
-void sim_loop(SimData& sim) {
-	int pop_score[POPULATION_SIZE];
-	int lowest_score = 1024;
-	int count = 0;
-	initialize_population(sim.words);
-
-	while(true) {
-		//cout << "scoring current population" << endl;
-		score_population(sim, pop_score);
-		//cout << "reporing score" << endl;
-		//report_score(pop_score);
-		//cout << "---------------" << endl;
-		int new_low_index = find_low(pop_score);
-
-		if (new_low_index<lowest_score) {
-			lowest_score = pop_score[sim.current_low_index];
-			sim.current_low_index = new_low_index;
-			sim.current_low = population[sim.current_low_index];
-			sim.current_low_score = pop_score[sim.current_low_index];
-			write_current_low(sim);
-		}
-
-		setup_new_population(sim);
-
-		count++;
-		if (count>CUTOFF) break;
-	}
-
-	cout << "final low is " << sim.current_low << endl;
-	cout << "score for final low is " << sim.current_low_score << endl;
-}
-
 int main() {
 	init();
 	SimData sim;
 	sim.load_data();
 
-	cout << "total number of words loaded: " << sim.words.size() << endl;
-	cout << "setting up random population..." << endl;
-
-	string target = make_target_string(sim.words);
+	string target = sim.make_target_string();
 	cout << "target is set to: " << target << endl;
-	SHA target_sha = make_target_sha(target);
+	SHA target_sha = sim.make_target_sha();
 	cout << "target sha is: " << target_sha << endl;
-	sim.target = target;
-	sim.target_sha = target_sha;
-
-	sim_loop(sim);
+	
+	sim.sim_loop();
 
 	return 0;
 }
